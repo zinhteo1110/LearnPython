7b9d5a847b9eca2baad3cff1c8aa71e88f501930
zinhteo1110 <duongngochai3000@gmail.com>
2017-05-24 15:04:56 +0700

    update code ex20.py

--- ex20.py
+++ ex20.py
@@ -6,11 +6,18 @@ script, input_file = argv
 def print_all(f):
     print f.read()
 
+# Each time you do f.seek(0) you're moving to the start of the file.
 def rewind(f):
     f.seek(0)
 
+#Each time you do f.readline() you're reading a line from the file,
+#and moving the read head to right after the \n that ends that line.
 def print_a_line(line_count, f):
     print line_count, f.readline()
+
+# Test readline() hoat dong doc lap.
+def print_a_line_1(f):
+    print f.readline()
 
 current_file = open(input_file)
 
@@ -24,8 +31,15 @@ rewind(current_file)
 
 print "Let's print three lines:"
 
-current_line = 1
+#current_line = 1
+current_line = 7
+# ==> readline() hoat dong doc lap. Ko lien quan den 'line_count' o tren.
 print_a_line(current_line, current_file)
 
 current_line = current_line + 1
-print_a_line(current_line, current_file)    
+print_a_line(current_line, current_file)
+
+rewind(current_file)
+print_a_line_1(current_file)
+print_a_line_1(current_file)
+print_a_line_1(current_file)
